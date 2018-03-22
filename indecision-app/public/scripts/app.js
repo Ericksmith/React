e1a8624f09502ac5eddd5b8512d2261484a58156
Erick Smith <smith.s.erick@gmail.com>
2018-03-21 22:04:56 -0700

    Modified jsx

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -3,10 +3,40 @@
 console.log('app.js is running');
 
 var template = React.createElement(
-  'p',
-  null,
-  'This is JSX from app.js!'
+    'div',
+    null,
+    React.createElement(
+        'p',
+        null,
+        'Does this change?'
+    ),
+    React.createElement(
+        'h1',
+        null,
+        'test'
+    )
 );
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Name:'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age:'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location:'
+    )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,6 +1,20 @@
 console.log('app.js is running');
 
-var template = <p>This is JSX from app.js!</p>
+var template = (
+    <div>
+        <p>Does this change?</p>
+        <h1>test</h1>
+    </div>
+);
+
+var templateTwo = (
+    <div>
+        <h1>Name:</h1>
+        <p>Age:</p>
+        <p>Location:</p>
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
