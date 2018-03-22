aace7047e301ca3e6515208147780fb40a07b190
Erick Smith <smith.s.erick@gmail.com>
2018-03-21 21:39:00 -0700

    added scripts

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,10 @@
+console.log('app.js is running');
+
+var template = React.createElement(
+    "h1",
+    { id: "someid" },
+    "Something new"
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
