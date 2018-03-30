58abe85960b9fc69821bbc7f638ab885a243c818
Erick Smith <smith.s.erick@gmail.com>
2018-03-29 17:16:17 -0700

    added react components

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,5 +1,52 @@
 class Header extends React.Component {
     render() {
-        return <p>This is from Header</p>
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>Put your life in the hands of a computer</h2>
+            </div>
+        )
     }
 }
+
+class Action extends React.Component {
+    render(){
+        return (
+            <div>
+                <button>What Should i Do</button>
+            </div>
+        )
+    }
+}
+
+class Options extends React.Component {
+    render(){
+        return (
+            <div>
+                <p>Options</p>
+            </div>
+        )
+    }
+}
+
+class AddOption extends React.Component {
+    render(){
+        return (
+            <div>
+                <p>AddOption</p>
+            </div>
+        )
+    }
+}
+
+const jsx = (
+    <div>
+        <h1>Title</h1>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+    </div>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
