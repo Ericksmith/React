ded7408ba382ed885af5aec8360ff2f2b31cdd20
Erick Smith <smith.s.erick@gmail.com>
2018-03-31 09:42:22 -0700

    options adding correctly

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,8 +3,9 @@ class IndecisionApp extends React.Component {
         super(props);
         this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
         this.handlePick = this.handlePick.bind(this);
+        this.handleAddOption = this.handleAddOption.bind(this);
         this.state = {
-            options: ['test', 'test2']
+            options: []
         };
     }
     
@@ -22,6 +23,19 @@ class IndecisionApp extends React.Component {
         alert(option);
     }
 
+    handleAddOption(option){
+        if(!option){
+            return 'Enter valid value to add item'
+        } else if(this.state.option.indexOf(option) > -1){
+            return 'This option already exists'
+        }
+        this.setState((prevState) => {
+            return {
+                options: prevState.options.concat(option)
+            }
+        });
+    }
+
     render(){
         const title = "Indecision App"
         const subtitle = 'Put your life in the hands of a computer'
@@ -31,7 +45,7 @@ class IndecisionApp extends React.Component {
                 <Header title={title} subtitle={subtitle} />
                 <Action hasOptions={this.state.options.length > 0} handlePick={this.handlePick} />
                 <Options options={this.state.options} handleDeleteOptions={this.handleDeleteOptions}/>
-                <AddOption />
+                <AddOption handleAddOption={this.handleAddOption} />
             </div>
         )
     }
@@ -89,12 +103,23 @@ class Options extends React.Component {
 }
 
 class AddOption extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleAddOption = this.handleAddOption.bind(this)
+        this.state = {
+            error: undefined
+        };
+    }
     handleAddOption(e){
         e.preventDefault();
         const option = e.target.elements.option.value.trim();
-        if(option){
-            alert(option)
+        const error = this.props.handleAddOption(option)
+        if(error){
+
         }
+        this.setState(()=> {
+            return { error }
+        });
     }
 
     render(){
