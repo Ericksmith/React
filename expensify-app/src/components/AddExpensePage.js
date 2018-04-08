830935c77a7071dc306df785c8dd9fb7550018df
Erick Smith <smith.s.erick@gmail.com>
2018-04-07 19:42:57 -0700

    react components created

---
+++ expensify-app/src/components/AddExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AddExpensePage = () => (
+    <div>
+        <p>This is from my add expense comp</p>
+    </div>
+);
+
+export default AddExpensePage;
---
+++ expensify-app/src/components/EditExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditExpensePage = () => (
+    <div>
+        <p>This is from my edit expense comp</p>
+    </div>
+);
+
+export default EditExpensePage;
