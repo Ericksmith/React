9585ae0413f74227d8a52dcf08ee4cc72e1afebd
Erick Smith <smith.s.erick@gmail.com>
2018-04-10 16:37:52 -0700

    array destructuring

--- portfolio-app/src/playground/destructuring.js
+++ portfolio-app/src/playground/destructuring.js
@@ -16,14 +16,28 @@
 // const {city, temp: temperature} = person.location;
 // console.log(`It's ${temperature} in ${city}.`);
 
-const book = {
-    title: 'Pillars of earth',
-    author: 'Ken Blah',
-    publisher: {
-        name: 'Penguin'
-    }
-}
+// const book = {
+//     title: 'Pillars of earth',
+//     author: 'Ken Blah',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// }
 
-const {name: publisherName = 'Self-Published'} = book.publisher;
+// const {name: publisherName = 'Self-Published'} = book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
+
+//
+//  array destructuring
+// 
+
+const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147']
+
+const [, city, state = 'New York'] = address;
+
+console.log(`You are in ${city} ${state}`);
+
+const item = ['coffee', '$2', '$2.50', '3.50']
+const [drink, small, medium, large] = item;
+console.log(`A medium ${drink} costs ${medium}`);
