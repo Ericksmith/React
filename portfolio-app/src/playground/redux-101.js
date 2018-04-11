30421993b3e00cbcd4c290dce69ef4726a34b296
Erick Smith <smith.s.erick@gmail.com>
2018-04-10 20:57:33 -0700

    exensify reducer start

--- portfolio-app/src/playground/redux-101.js
+++ portfolio-app/src/playground/redux-101.js
@@ -1,6 +1,31 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = {count: 0}, action) => {
+
+
+const incrementCount = ({incrementBy = 1} = {}) => ({
+    type: 'INCREMENT',
+    incrementBy
+});
+
+const decrementCount = ({decrementBy = 1}= {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+});
+
+const setCount = ({count}) => ({
+    type: 'SET',
+    count
+})
+
+const resetCount = () => ({
+    type: 'RESET'
+});
+
+//Reducer
+// 1. Reducers are pure functions, they don't user variables from outside thier scope
+// 2. Never change state or action
+
+const countReducer = (state = {count: 0}, action) => {
     switch(action.type){
         case 'INCREMENT':
             const incrementBy = typeof(action.incrementBy) === 'number' ? action.incrementBy : 1; 
@@ -23,11 +48,18 @@ const store = createStore((state = {count: 0}, action) => {
         default:
             return state; 
     } 
-})
+}
+
+const store = createStore(countReducer);
 
 store.subscribe(() => {
     console.log('sub', store.getState());
 });
+
+store.dispatch(incrementCount({incrementBy: 10000}));
+store.dispatch(decrementCount({decrementBy: 5000}));
+store.dispatch(resetCount());
+store.dispatch(setCount({count: 69}));
 
 
 store.dispatch({
