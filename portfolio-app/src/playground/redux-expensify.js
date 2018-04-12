decb0524dc85cecbc75f5a935cfd1da5bb3822d8
Erick Smith <smith.s.erick@gmail.com>
2018-04-11 20:18:11 -0700

    Added uuid

--- portfolio-app/src/playground/redux-expensify.js
+++ portfolio-app/src/playground/redux-expensify.js
@@ -1,6 +1,59 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
-const demo = {
+
+const addExpense = (
+    { 
+        description = '', 
+        note = '', 
+        amount = 0, 
+        createdAt = 0 
+    }) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+})
+
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch(action.type) {
+        
+        default:
+            return state;
+    }
+};
+
+const filterReducerDefaultState = { text: '', sortBy: 'date', startDate: undefined, endDate: undefined };
+
+const filterReducer = (state = filterReducerDefaultState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filterReducer
+    })
+);
+
+store.subscribe(()=>{
+    console.log(store.getState());
+});
+
+store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+
+console.log(store.getState());
+
+const demoState = {
     expenses: [{
         id: 'adsfnandsfwrea',
         description: 'January Rent',
