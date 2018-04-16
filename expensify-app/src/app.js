2b988579638c344e86d2a0ec17058146f716740e
Erick Smith <smith.s.erick@gmail.com>
2018-04-16 16:51:31 -0700

    add hoc example created

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter'
-import configureStore from './store/configureStore'
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -9,7 +12,10 @@ const store = configureStore();
 
 store.dispatch(addExpense({description: 'water bill', amount: 10000, createdAt: -21000}));
 store.dispatch(addExpense({description: 'gas bill', amount: 10000, createdAt: -21000}));
+store.dispatch(setTextFilter('water'));
 
-console.log(store.getState());
+const state = store.getState();
+const visableExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visableExpenses);
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
